1a735057ec484762a224329fbdcd6c374e6d00f9
rb_de0 <rebirth.de0@gmail.com>
2016-08-15 15:34:36 +0900

    Fix parsing logic.

--- Sources/Parser.swift
+++ Sources/Parser.swift
@@ -3,28 +3,39 @@ import Foundation
 
 public class Parser{
     
-    class func toHtml(markdown: String) -> String{
-        let document = mkd_string(markdown, Int32(markdown.lengthOfBytes(using: NSUTF8StringEncoding)), 0)
+    private typealias MarkdownPointer = UnsafeMutablePointer<Void>
+    private typealias ExtractFunc = (MarkdownPointer, UnsafeMutablePointer<UnsafeMutablePointer<Int8>?>) -> Int32
+    
+    public class func toHtml(markdown: String) -> String{
+        
+        let mkPointer = mkd_string(markdown, Int32(markdown.lengthOfBytes(using: NSUTF8StringEncoding)), 0)
         
         defer{
-            mkd_cleanup(document)
+            mkd_cleanup(mkPointer)
         }
         
-        mkd_compile(document, 0)
+        mkd_compile(mkPointer, 0)
         
-        var output = [UnsafeMutablePointer<Int8>?](repeating: UnsafeMutablePointer<Int8>(allocatingCapacity: 1), count: 1)
-        mkd_document(document, &output)
-        
-        if output.isEmpty{
-            return ""
-        }
-        
-        guard let html = output[0] else{
-            return ""
-        }
-        
-        let result = String(cString: html)
-        return result
+        return extractFromMarkdown(mkPointer: mkPointer, extract: mkd_document)
     }
     
+}
+
+// MARK: - Private Method
+extension Parser{
+    
+    private class func extractFromMarkdown(mkPointer: MarkdownPointer?, extract: ExtractFunc) -> String{
+        guard let pointer = mkPointer else{
+            return ""
+        }
+        
+        var output: [UnsafeMutablePointer<Int8>?] = [UnsafeMutablePointer<Int8>(allocatingCapacity: 1)]
+        extract(pointer, &output)
+        
+        guard let html = output.first.flatMap({$0}) else{
+            return ""
+        }
+        
+        return String(cString: html)
+    }
 }
